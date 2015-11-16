fecc8970751274b9870d71ff7623f921832d384c
k-liao <kpl44@cornell.edu>
2015-11-16 17:30:56 -0500

    Added builder.ml

---
+++ builder.ml
@@ -0,0 +1,10 @@
+open Yojson.Basic.Util
+open Types
+
+(*builds a JSON file from the given db*)
+let build (db:db) : Yojson.Basic.json =
+  failwith "unimplemented"
+
+(*saves the JSON file to a given file location, overwrites old JSON*)
+let save (db:db) (f_name:string) : unit =
+  failwith "unimplemented"
