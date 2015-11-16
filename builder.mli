42cfd0943ea72fa5768d69352dcf8cc2be8ba6b8
k-liao <kpl44@cornell.edu>
2015-11-16 17:27:05 -0500

    Open Yojson.Basic.Util correctly

--- builder.mli
+++ builder.mli
@@ -1,8 +1,8 @@
-open Yojson
+open Yojson.Basic.Util
 open Types
 
 (*builds a JSON file from the given db*)
-val build : db -> Yojson.Basic.Json
+val build : db -> Yojson.Basic.json
 
 (*saves the JSON file to a given file location, overwrites old JSON*)
 val save : db -> string -> unit
