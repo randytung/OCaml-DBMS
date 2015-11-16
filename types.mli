aec380a5852f1e73b6c0ddf21c7927a4dc51c745
Grant Stento <grantstento@dhcp-gs-1841.eduroam.cornell.edu>
2015-11-16 18:25:03 -0500

    fix types

--- types.mli
+++ types.mli
@@ -1,4 +1,9 @@
-val value : 'a
-val category : {name : string; vals : value list; type : string}
-val table : {name : string; cats : category list}
-val db : table list
+(** the type of a category *)
+type val_type =
+  | String of string
+  | Integer of int
+  | Float of float
+
+type category = {name : string; vals : 'a list; type : val_type}
+type table = {name : string; cats : category list}
+type db = table list
