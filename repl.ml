fb57c48dfa7c1d81c3697b7957ed533221c7a61c
Randy Tung <randytung8@hotmail.com>
2015-11-16 17:32:17 -0500

    added repl.ml

---
+++ repl.ml
@@ -0,0 +1,9 @@
+open Operations
+open Builder
+open Types
+
+(*prompts for user input, calls operations, saves the database using builder, and
+* recursively initializes itself with the revised db until user input causes the
+* loop to halt*)
+let rec loop  (db:db) (input:string) : unit =
+  failwith "hehe"
