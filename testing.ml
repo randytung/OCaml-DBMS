26dd40abbaee43528da27be3400b1ee0e37bd7b5
arthur-wang <aw592@cornell.edu>
2015-12-03 18:27:30 -0500

    added tests for delete, select exception tests

--- testing.ml
+++ testing.ml
@@ -3,45 +3,35 @@ open Operations
 open Assertions
 
 (* Tests for accuracy *)
-(*demodb is an example database taken from http://www.w3schools.com/sql/sql_select.asp*)
-let demodb = [{title="Customers";cols=[{name="CustomerID";vals=[VInt(1);VInt(2);VInt(3);VInt(4);VInt(5)];typ=TInt};
-              {name="CustomerName";vals=[VString("Alfreds Futterkiste");VString("Ana Trujillo Emparedados y helados");VString("Antonio Moreno Taqueria");VString("Around the Horn");VString("Berglunds snabbkop")];typ=TString};
-              {name="ContactName";vals=[VString("Maria Anders");VString("Ana Trujillo");VString("Antonio Moreno");VString("Thomas Hardy");VString("Christina Berglund")];typ=TString};
-              {name="Address";vals=[VString("Obere Str. 57");VString("Avda. de la Constitucion 2222");VString("Mataderos 2312");VString("120 Hanover Sq.");VString("Berguvsvagen 8")];typ=TString};
-              {name="City";vals=[VString("Berlin");VString("Mexico D.F.");VString("Mexico D.F.");VString("London");VString("Lulea")];typ=TString};
-              {name="PostalCode";vals=[VString("12209");VString("05021");VString("05023");VString("WA1 1DP");VString("S-958 22")];typ=TString}]}]
-TEST "Select" = (select (demodb) "CustomerName,City FROM Customers") = demodb
-
-
 
 let names = {name="Name"; vals=[VString "Arthur"; VString "Grant"]; typ=TString}
-let nums = {name="Number"; vals=[VNull; VInt 6; VInt 5]; typ=TInt}
+let nums = {name="Number"; vals=[VInt 6; VInt 5]; typ=TInt}
 
 let db = [{title="Phonebook"; cols=[names; nums]}]
 
+TEST "Select" = (select (db) "Name,Number FROM Phonebook") = db
 
 TEST "Create" =
   (*test with correct input*)
   let db_tbl = create db "Student (Name string,netid string)" in
   let name =  {name="Name"; vals=[]; typ=TString} in
   let netid = {name="netid"; vals=[]; typ=TString} in
-  let check_db = [{title="Student"; cols = [name;netid]};{title="Phonebook";
+  let check_db = [{title="Student"; cols = [netid;name]};{title="Phonebook";
     cols=[names; nums]} ] in
-  let test = check_db = db_tbl in
+  let _ = check_db = db_tbl in
   (*test with empty columns*)
   let db_tbl2 = create db "People ()" in
     let check_db2 = [{title="People"; cols = []};{title="Phonebook";
     cols = [names; nums]}] in
-  let test2 = check_db2 = db_tbl2 in
+  let _ = check_db2 = db_tbl2 in
   (*testing with existing table name*)
-  let test3 = try let _ = create db "Phonebook ()" in false
+  let test1 = try let _ = create db "Phonebook ()" in false
               with _ -> true in
+  let _ = test1 in
   (*testing with nonexisting type*)
-  let test4 = try let _  = create db "Person (Name int, Number cheese)" in false
+  let test2 = try let _  = create db "Person (Name int, Number cheese)" in false
               with _ -> true in
-  test2 && test3 && test4 && test
-
-
+  test2
 
 let add_val col v =
   let new_vals = col.vals @ [v] in
@@ -74,7 +64,8 @@ let stuff5 = {name="Stuff 5"; vals=[VNull; VNull]; typ=TFloat}
 let big_db = [{title="Stuffs"; cols=[stuff1; stuff2; stuff3; stuff4; stuff5]}]
 
 TEST "Insert with multiple specified columns" =
-  let db' = insert big_db "Stuffs (Stuff 2, Stuff 5, Stuff 4, Stuff 3) VALUES (1, 3.4, true, false)" in
+  let db' = insert big_db "Stuffs (Stuff 2, Stuff 5, Stuff 4, Stuff 3) VALUES
+            (1, 3.4, true, false)" in
   let stuff1' = add_val stuff1 VNull in
   let stuff3' = add_val stuff3 (VBool false) in
   let stuff2' = add_val stuff2 (VInt 1) in
@@ -82,47 +73,32 @@ TEST "Insert with multiple specified columns" =
   let stuff4' = add_val stuff4 (VBool true) in
   db' = [{title="Stuffs"; cols=[stuff1'; stuff2'; stuff3'; stuff4'; stuff5']}]
 
-let names = {name="Name"; vals=[VString "Arthur"; VString "Grant"]; typ=TString}
-let nums = {name="Number"; vals=[VNull; VInt 6; VInt 5]; typ=TInt}
+TEST "Update" = failwith ""
 
-let db = [{title="Phonebook"; cols=[names; nums]}]
+let delnames = {name="Name"; vals=[VString "Arthur"; VString "Arthur";
+  VString "Grant"]; typ=TString}
+let delnums = {name="Number"; vals=[VInt 5; VInt 6; VInt 5]; typ=TInt}
 
+let deldb = [{title="Phonebook"; cols=[delnames; delnums]}]
 
-TEST "Update" =
-  (*regular input of update*)
-  let test1 = update db "Phonebook Set Name = 'Wang' WHERE Number = 6" in
-  let names1 = {name="Name"; vals=[VString "Wang"; VString "Grant"]; typ=TString} in
-  let testdb1 = [{title="Phonebook"; cols=[nums;names1]}] in
-  test1 = testdb1
+TEST "DeleteAND" = (delete db "Phonebook where Name = 'Arthur' AND Number = 5") =
+  ([{title="Phonebook";cols=[{name="Name"; vals=[VString "Arthur"; VString "Grant"];
+    typ=TString}; {name="Number"; vals=[VInt 6; VInt 5];typ=TInt}]}])
 
+TEST "DeleteOR" = (delete db "Phonebook where Name = 'Arthur' OR Number = 5") =
+  ([{title="Phonebook";cols=[{name="Name"; vals=[];
+    typ=TString}; {name="Number"; vals=[];typ=TInt}]}])
 
-
-
-
-
-TEST "DeleteAND" = (delete demodb "Customers
-  WHERE CustomerName='Alfreds Futterkiste' OR ContactName='Christina Berglund'") =
-  ([{title="Customers";cols=[{name="CustomerID";vals=[VInt(2);VInt(3);VInt(4);VInt(5)];typ=TInt};
-  {name="CustomerName";vals=[VString("Ana Trujillo Emparedados y helados");VString("Antonio Moreno Taqueria");VString("Around the Horn");VString("Berglunds snabbkop")];typ=TString};
-  {name="ContactName";vals=[VString("Ana Trujillo");VString("Antonio Moreno");VString("Thomas Hardy");VString("Christina Berglund")];typ=TString};
-  {name="Address";vals=[VString("Avda. de la Constitucion 2222");VString("Mataderos 2312");VString("120 Hanover Sq.");VString("Berguvsvagen 8")];typ=TString};
-  {name="City";vals=[VString("Mexico D.F.");VString("Mexico D.F.");VString("London");VString("Lulea")];typ=TString};
-  {name="PostalCode";vals=[VString("05021");VString("05023");VString("WA1 1DP");VString("S-958 22")];typ=TString}]}]);
-
-TEST "DeleteOR" = (delete demodb "Customers
-  WHERE CustomerName='Alfreds Futterkiste' AND ContactName='Maria Anders'") =
-  ([{title="Customers";cols=[{name="CustomerID";vals=[VInt(2);VInt(3);VInt(4)];typ=TInt};
-  {name="CustomerName";vals=[VString("Ana Trujillo Emparedados y helados");VString("Antonio Moreno Taqueria");VString("Around the Horn")];typ=TString};
-  {name="ContactName";vals=[VString("Ana Trujillo");VString("Antonio Moreno");VString("Thomas Hardy")];typ=TString};
-  {name="Address";vals=[VString("Avda. de la Constitucion 2222");VString("Mataderos 2312");VString("120 Hanover Sq.")];typ=TString};
-  {name="City";vals=[VString("Mexico D.F.");VString("Mexico D.F.");VString("London")];typ=TString};
-  {name="PostalCode";vals=[VString("05021");VString("05023");VString("WA1 1DP")];typ=TString}]}])
-
-let drop_db = [{title="Customers";cols=[{name="CustomerID";vals=[VInt(1);VInt(2);VInt(3);VInt(4);VInt(5)];typ=TInt};
-              {name="CustomerName";vals=[VString("Alfreds Futterkiste");VString("Ana Trujillo Emparedados y helados");VString("Antonio Moreno Taqueria");VString("Around the Horn");VString("Berglunds snabbkop")];typ=TString};
-              {name="ContactName";vals=[VString("Maria Anders");VString("Ana Trujillo");VString("Antonio Moreno");VString("Thomas Hardy");VString("Christina Berglund")];
+let drop_db = [{title="Customers";cols=[{name="CustomerID";vals=[VInt(1);VInt(2);
+              VInt(3);VInt(4);VInt(5)];typ=TInt};{name="CustomerName";
+              vals=[VString("Alfreds Futterkiste");
+              VString("Ana Trujillo Emparedados y helados");
+              VString("Antonio Moreno Taqueria");VString("Around the Horn");
+              VString("Berglunds snabbkop")];typ=TString};
+              {name="ContactName";vals=[VString("Maria Anders");
+              VString("Ana Trujillo");VString("Antonio Moreno");
+              VString("Thomas Hardy");VString("Christina Berglund")];
               typ=TString}]};{title="Phonebook"; cols=[names; nums]}]
-
 
 TEST "Drop" = let new_db = drop drop_db "Customers" in
   let dropped_db = [{title="Phonebook"; cols=[names; nums]}] in
@@ -130,16 +106,15 @@ TEST "Drop" = let new_db = drop drop_db "Customers" in
   let next_db = drop new_db "Phonebook" in
   next_db = []
 
-
 let col1 = {name = "Name"; vals = [VString "Grant Stento"; VString "Kristy Liao"];
-       typ = TString}
+            typ = TString}
 let col2 = {name = "Phone_Number"; vals = [VInt 456; VInt 654]; typ = TInt}
 
 let db = [{title = "Phone_book"; cols = [col1;col2]}]
 
 TEST "Alter_Add" = let new_db = alter db "Phone_book ADD Address string" in
   let col3 = {name = "Address"; vals = [VNull; VNull]; typ = TString} in
-  let add_db = [{title = "Phone_book"; cols = [col1;col2;col3]}] in
+  let add_db = [{title = "Phone_book"; cols = [col3;col1;col2]}] in
   new_db = add_db
 
 TEST "Alter Drop" = let new_db = alter db "Phone_book DROP COLUMN Name" in
@@ -148,7 +123,7 @@ TEST "Alter Drop" = let new_db = alter db "Phone_book DROP COLUMN Name" in
 
 TEST "Alter Modify with correct type" =
   let new_db = alter db "Phone_book MODIFY COLUMN Phone_Number float" in
-  let col3 = {name = "Phone_Number"; vals = [VFloat 456.; VFloat 654.]; typ = TFloat} in
+  let col3 = {name="Phone_Number";vals=[VFloat 456.; VFloat 654.];typ=TFloat} in
   let modify_db = [{title = "Phone_book"; cols = [col1;col3]}] in
   modify_db = new_db
 
@@ -158,7 +133,13 @@ TEST "Alter Modify with incorrect type" =
   let modify_db = [{title = "Phone_book"; cols = [col3;col2]}] in
   new_db = modify_db
 
-
 (* Tests for exceptions *)
-TEST_UNIT "Select" = failwith ""
-TEST_UNIT "Insert" = failwith ""
+TEST "Select with wrong column name" =
+  try let _ = select db "Phonebook from Phonebook" in false
+  with _ -> true
+
+TEST "Select with wrong table name" =
+  try let _ = select db "Name from Number" in false
+  with _ -> true
+
+TEST "InsertException" = failwith ""
